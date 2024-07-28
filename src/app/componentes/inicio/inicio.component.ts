204aa6c70d1632bbd9b5445f9341464c84e45acc
Tobias <tobiasibarra4@gmail.com>
2024-07-28 10:57:21 -0300

    Portafolio

--- src/app/componentes/inicio/inicio.component.ts
+++ src/app/componentes/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, HostListener, Inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import * as AOS from 'aos'; // Ajuste en la importación
 import 'aos/dist/aos.css';
 
@@ -10,12 +11,33 @@ import 'aos/dist/aos.css';
   styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
+ 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-  descargarCV() {
-    const link = document.createElement('a');
-    
-    link.href = 'assets/TobiasIbarra_CV.pdf';  
-    link.download = 'TobiasIbarra_CV.pdf'; 
-    link.click();
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      AOS.init();
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const navbar = document.getElementById('myTopnav');
+      if (window.pageYOffset > 0) {
+        navbar?.classList.add('fixed');
+      } else {
+        navbar?.classList.remove('fixed');
+      }
+    }
+  }
+
+  descargarCV(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      const link = document.createElement('a');
+      link.href = 'assets/TobiasIbarra_CV.pdf';
+      link.download = 'TobiasIbarra_CV.pdf';
+      link.click();
+    }
   }
 }
